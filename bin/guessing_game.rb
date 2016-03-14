739abe20d06064d38ff520c3c254a0f40fc26967
Dane Carmichael <carmichaeldane@gmail.com>
2016-03-14 14:56:38 -0500

    "Tests to see if your new guess is equal to your old guess and breaks the cycle."

--- bin/guessing_game.rb
+++ bin/guessing_game.rb
@@ -1,10 +1,24 @@
 require_relative '../lib/computer_number.rb'
 require_relative '../lib/number_of_guesses.rb'
+require_relative '../lib/repeat_guess.rb'
 
 class GuessingGame
   puts "Hello, let us play a guessing game. I will pick a number 1-100, what is it?"
   number = ComputerNumber.new
-  human_guess = gets.chomp.to_i
   guesses = NumberOfGuesses.new
-  guesses.user_guess.push(human_guess)
+  repeat = RepeatGuess.new
+  old_guess = repeat.old_guess
+  puts old_guess
+  human_guess = gets.chomp.to_i
+  loop do
+    puts "Human guess: #{human_guess} Old Guess: #{old_guess}"
+    if human_guess == old_guess
+      puts "You have guessed this number already, please guess again."
+      human_guess = gets.chomp.to_i
+      break
+    else
+      guesses.user_guess.push(human_guess)
+      old_guess = human_guess
+    end
+  end
 end
---
+++ lib/repeat_guess.rb
@@ -0,0 +1,7 @@
+
+class RepeatGuess
+  attr_accessor :old_guess
+  def initialize
+    @old_guess = 0
+  end
+end
