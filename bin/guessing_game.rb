ddec725653d5c2ddf9be026b2e9f05aebf50afa6
Dane Carmichael <carmichaeldane@gmail.com>
2016-03-15 08:06:40 -0500

    Half-way through adventure mode."

--- bin/guessing_game.rb
+++ bin/guessing_game.rb
@@ -5,19 +5,20 @@ require_relative '../lib/direct_guesses.rb'
 
 class GuessingGame
   puts "Hello, let us play a guessing game. I will pick a number 1-100, what is it?"
+  puts "You only have five chances."
   number = ComputerNumber.new
   guesses = NumberOfGuesses.new
   repeat = RepeatGuess.new
   direction = DirectGuesses.new(0,number.computer_number)
   old_guess = repeat.old_guess
-  puts old_guess
   loop do
     human_guess = gets.chomp.to_i
-    puts "Human guess: #{human_guess} Old Guess: #{old_guess}"
     if human_guess == old_guess
-      puts "You have guessed this number already, please guess again."
+      puts "You have guessed this number already, this was a waste of a guess."
+      guesses.user_guess.push(human_guess)
     else
       guesses.user_guess.push(human_guess)
+      puts "Your guesses so far #{guesses.user_guess * ","}"
       old_guess = human_guess
     end
     direction.user = human_guess
@@ -26,6 +27,7 @@ class GuessingGame
       break
     elsif guesses.user_guess.length == 5
       puts "Sorry, you have guessed too any times. You lose."
+      puts "For posterities sake the number was #{number.computer_number}"
       break
     else
       puts "#{direction.provide_direction}"
